4a8cea65ffc21dbdd64789902a4e3514a8b58e77
Harshit Shetty <shettyharshit5@gmail.com>
2018-03-01 17:49:01 +0530

    Works

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,6 @@ class User < ApplicationRecord
   has_attached_file :image, 
       :styles => { :medium => "300x300>" }, 
       :default_url => "/images/:style/missing.png" # <= you see ?
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  # validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  validates_attachment :image, :content_type => { :content_type => ["application/octet-stream","image/jpg","image/jpeg","image/png"] }
 end
